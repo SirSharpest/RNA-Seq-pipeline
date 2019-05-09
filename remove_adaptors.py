8af80187eb0c5de3744689bc36920443d2c61107
Nathan Hughes <nathan1hughes@gmail.com>
2019-05-09 10:36:03 +0100

    Small bug with adaptor removal not working

--- remove_adaptors.py
+++ remove_adaptors.py
@@ -48,9 +48,10 @@ def main():
         out_f = "{0}/trimmed_{1}".format(args['FORWARDOUT'], basename(f))
         out_r = "{0}/trimmed_{1}".format(args['REVERSEOUT'], basename(r))
         cmd = "srun java -jar '/nbi/software/testing/trimmomatic/0.33/x86_64/bin/trimmomatic-0.33.jar' PE -threads {5} \
-             -phred33 {0} {1} {2} {3} ILLUMINACLIP:{4}:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36".format(f, r,
-                                                                                                                         out_f, out_r,
-                                                                                                                         adaptors, threads)
+        -phred33 {0} {1} {2} {3} unpaired_unaligned_1.fq.gz unpaired_unaligned_2.fq.gz \
+        ILLUMINACLIP:{4}:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36".format(f, r,
+                                                                                           out_f, out_r,
+                                                                                           adaptors, threads)
         run(cmd, shell=True)
 
 
